4135996dbeaac1111f465cf27654c0a67317365d
RAJESH <rmurugan3174@gmail.com>
2025-07-30 18:12:01 +0530

    integer,floar,char,string,cin

---
+++ ex2.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //integer,float,char,string
+    int a;
+    float b;
+    char c;
+    string d;
+    //scanf - cin
+    cin>>a>>b>>c>>d;
+    //printf - cout
+    cout<<a<<endl<<b<<endl<<c<<endl<<d;
+    return 0;
+}
