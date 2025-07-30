a04bf22b8085ad3e10e1b4269c06aaaec4728e04
RAJESH <rmurugan3174@gmail.com>
2025-07-30 18:02:27 +0530

    library,namespace,cout,endl

---
+++ ex1.cpp
@@ -0,0 +1,10 @@
+//import all library
+#include<bits/stdc++.h>
+//avoid muitiple times std
+using namespace std;
+
+int main(){
+    // cout-printf, endl-newline
+    cout<<"Rajesh"<<endl<<"Madesh";
+    return 0;
+}
