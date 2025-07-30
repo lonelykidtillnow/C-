758605da0b6e69d65a183a59f332d9b339368c81
RAJESH <rmurugan3174@gmail.com>
2025-07-30 23:22:43 +0530

    Precision and width

---
+++ ex11.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   int n1,n2;
+   cin>>n1>>n2;
+   cout<<setfill('0')<<setw(5)<<n1<<endl;
+   cout<<setfill(' ')<<setw(5)<<n2;
+    return 0;
+}
