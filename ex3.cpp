b4b0453db3b02be52dec49da63ff975d359d7fcf
RAJESH <rmurugan3174@gmail.com>
2025-07-30 18:22:15 +0530

    char-'',string-,getline()

---
+++ ex3.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //char - ' '
+    char a='R';
+    //string - " "
+    string b="Rajesh";
+    
+    cout<<a<<endl<<b<<endl;
+    
+    string c;
+    //input: raj esh - output: raj esh
+    getline(cin,c);
+    cout<<c;
+    
+    return 0;
+}
