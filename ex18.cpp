396624ba82527fd97922b54aa7e32973094a20a9
RAJESH <rmurugan3174@gmail.com>
2025-07-31 09:26:52 +0530

    odd numbers

---
+++ ex18.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  
+  int n;
+  cin>>n;
+  
+  for(int i=1;i<n;i++){
+    if(i%2!=0)  cout<<i<<" ";
+  }
+  
+  return 0;
+}
+
+
+// Sample Input 
+// 10
+// Sample Output
+// 1 3 5 7 9
