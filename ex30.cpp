5cbdba8d27103a0e4164e8613f2f18a80f52fea7
RAJESH <rmurugan3174@gmail.com>
2025-07-31 22:49:37 +0530

    Solid Square Pattern

--- ex30.cpp
+++ ex30.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
   
-  for(int i=0;i<4;i++){    //i-Row
+  for(int i=0;i<4;i++){    //i-Row ,No.of lines=4 => Row=4 => i=0;i<4;i++
     for(int j=0;j<4;j++){  //j-Column
       cout<<"*";
     }
