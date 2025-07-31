0e4c53acf6f8dc57a2ab870f231d484348f27575
RAJESH <rmurugan3174@gmail.com>
2025-07-31 09:17:11 +0530

    Sum and Difference of Two Numbers

---
+++ ex17.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  
+  int n1,n2;
+  float n3,n4;
+  cin>>n1>>n2>>n3>>n4;
+  
+  cout<<n1+n2<<" "<<n1-n2<<endl;
+  
+  cout<<fixed<<setprecision(1)<<n3+n4<<" "<<n3-n4;
+  
+  return 0;
+}
+
+// Sample Input
+// 10 4
+// 4.0 2.0
+
+// Sample Output
+// 14 6
+// 6.0 2.0
