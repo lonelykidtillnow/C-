369ace7a85b4647867fb57cae887935bea176a92
RAJESH <rmurugan3174@gmail.com>
2025-08-01 12:54:57 +0530

    Double-Sided Hollow Diamond Pattern

---
+++ ex45.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    //Upper Part
+    for(int i=0;i<5;i++){
+      for(int j=0;j<5-i;j++){
+        cout<<"*";
+      }
+      for(int k=0;k<2*i;k++){
+        cout<<" ";
+      }
+      for(int j=5-i;j>0;j--){
+        cout<<"*";
+      }
+      cout<<endl;
+    }
+    
+    //Lower Part
+    for(int i=0;i<5;i++){
+      for(int j=0;j<i+1;j++){
+        cout<<"*";
+      }
+      for(int k=0;k<8-2*i;k++){
+        cout<<" ";
+      }
+      for(int j=i+1;j>0;j--){
+        cout<<"*";
+      }
+      cout<<endl;
+    }
+   
+    return 0;
+}
+
+
+// Output:
+// **********
+// ****  ****
+// ***    ***
+// **      **
+// *        *
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+// **********
