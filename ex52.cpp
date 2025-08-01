daef933542432d8b6628bcd08963e2fa6a86f7a0
RAJESH <rmurugan3174@gmail.com>
2025-08-01 15:29:15 +0530

    Recursive with Parameters , Pure Functional Recursion

---
+++ ex52.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//1.Factorial using Recursive with Parameters & Argument
+void sum(int a,int b=1,int add=0){
+  
+  if(a==b){
+    add=add+b;
+    cout<<add;
+    return;
+  }
+  add=add+b;
+  sum(a,b+1,add);
+}
+int main(){
+  int n;
+  cin>>n;
+  sum(n);
+  
+  return 0;
+}
+
+
+//2.Factorial using Pure Functional Recursion
+int sum(int a){
+  
+  if(a==1){
+    return 1;
+  }
+  return a+sum(a-1);
+}
+int main(){
+  int n;
+  cin>>n;
+  int res=sum(n);
+  cout<<res;
+  
+  return 0;
+}
