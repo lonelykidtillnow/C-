238ae5a3a4d4b254b48e5faf1289ddf55afaab11
RAJESH <rmurugan3174@gmail.com>
2025-09-02 21:43:11 +0530

    Pseudo code for & Switch

---
+++ ex66.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    for(int i=0;i<20;i++){
+      switch(i){
+        case 0: i+=5;
+        case 1: i+=2;
+        case 5: i+=5;
+        default: i+=4;
+      }
+      cout<<i<<" ";
+    }
+    
+    return 0;
+}
