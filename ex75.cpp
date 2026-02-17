29f58a074dcf8159d64e853e7df7d8c2abe462a4
RAJESH <rmurugan3174@gmail.com>
2026-02-17 20:58:44 +0530

    Largest & Second Largest Element in an Array

---
+++ ex75.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int arr[] = {10,50,100,100,80};
+    
+    int n=sizeof(arr)/sizeof(arr[0]);
+    
+    int largest=arr[0];
+    int second_largest=0;
+    
+    for(int i=1;i<=n-1;i++){
+        if(largest<arr[i]){
+            second_largest=largest;
+            largest=arr[i];
+        }
+        else if(second_largest<arr[i] && arr[i]!=largest){
+            second_largest=arr[i];
+        }
+    }
+    cout<<largest<<" "<<second_largest;
+    
+    return 0;
+}
+
+// 🧾 Dry run
+
+// largest=10 second=-∞
+
+// i=1 → 10<50
+// → second=10 largest=50
+
+// i=2 → 50<100
+// → second=50 largest=100
+
+// i=3 → 
+// if (100<80) no
+// else if (50<80 && 80!=100)
+// -> second=80
+
+// OUTPUT → 100 80
